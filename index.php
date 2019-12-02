4dc8b9b995753a3da232c733959e7749b5703313
bismaariefqi <bismaariefqi@gmail.com>
2019-12-02 15:16:27 +0700

    fix session login regis

--- index.php
+++ index.php
@@ -32,9 +32,9 @@
     
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
       <div class="container">
-        <a class="navbar-brand" href="index.html">E-bud</a>
+        <a class="navbar-brand" href="index.php">E-bud</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="oi oi-menu"></span> Menu
+          <span class="oi oi-menu"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="ftco-nav">
